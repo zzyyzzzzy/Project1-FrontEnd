5c1d20868d7931755d970b71d512fd560471f1e0
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-08-18 14:08:22 -0700

    finish update complaint

--- js/fetchUnsignedComplaints.js
+++ js/fetchUnsignedComplaints.js
@@ -9,24 +9,40 @@ const getComplaintlist = async () => {
     
 };
 
+const prorityMapper = priority => {
+    switch(priority) {
+        case "UASSIGNED":
+            return 0;
+        case "HIGH":
+            return 1;
+        case "LOW":
+            return 2;
+        default:
+            return 3;
+      }
+};
+
 const renderTable = async () => {
     const complaintList = await getComplaintlist();
+    complaintList.sort((c1, c2) => prorityMapper(c2.priority) - prorityMapper(c1.priority));
     for(let i = 0; i < complaintList.length; i++){
         const element = complaintList[i];
-        const {summary, priority, meetingId} = element;
-        if(priority == "UNASSIGNED"){
-            const element = complaintList[i];
+        const {id, summary, priority, meetingId} = element;
+        
         const newRow = table.insertRow();
         const cell1 = newRow.insertCell();
         const cell2 = newRow.insertCell();
         const cell3 = newRow.insertCell();
         const cell4 = newRow.insertCell();
- 
+        const cell5 = newRow.insertCell();
         const meetingAssigned = meetingId == -1 ? "Not Assigned" : `Assigned to Meeting ${meetingId}`;
         cell1.innerText= i + 1;
         cell2.innerText = summary;
         cell3.innerText = meetingAssigned;
-        
+        cell4.innerText = priority;
+
+
+        // creating buttons
         const btnHigh = document.createElement("button");
         btnHigh.innerText = "High";
         btnHigh.classList.add("btn", "btn-primary");
@@ -36,10 +52,31 @@ const renderTable = async () => {
         const btnIgnore = document.createElement("button");
         btnIgnore.innerText = "Ignore";
         btnIgnore.classList.add("btn", "btn-secondary", "ms-2");
-        cell4.appendChild(btnHigh);
-        cell4.appendChild(btnLow);
-        cell4.appendChild(btnIgnore);
+
+        //setup click listener for buttons
+        const btnCallBack = async e => {
+            const response = await fetch(`${baseUrl}/complaints/${id}/${e.target.innerText}`, {
+                method: "PATCH",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            });
+            if (response.status == 200){
+                const updatedBody = await response.json();
+                cell4.innerText = updatedBody.priority;
+            } else{
+                console.error(response);
+                alert("Somthing wet wrong during update the prority")
+            }
         }
+        btnHigh.addEventListener("click", btnCallBack);
+        btnLow.addEventListener("click", btnCallBack);
+        btnIgnore.addEventListener("click", btnCallBack);
+        cell5.appendChild(btnHigh);
+        cell5.appendChild(btnLow);
+        cell5.appendChild(btnIgnore);
+        
+
     }
 }
 
